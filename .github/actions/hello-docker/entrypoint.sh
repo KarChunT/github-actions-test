92021ae0975beab047a26f986621b42c6e2261e0
Kar Chun Tan <kar.chun.tan@intel.com>
2022-07-26 15:50:17 +0800

    docker

---
+++ .github/actions/hello-docker/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# debug message
+echo "::debug::Debug message"
+echo "::warning::Warning message"
+echo "::error::error message"
+
+# set secret
+echo "::add-mask::$1"
+echo "Hello $1"
+time=$(date)
+# set output
+echo "::set-output name=time::$time"
+
+# grouping
+echo "::group::Some expandable logs"
+echo "some stuff"
+echo "good"
+echo "::endgroup::"
+
+# set env value
+echo "::set-env name=HELLO::envvalue"
